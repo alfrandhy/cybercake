dd3750186e80cbe89527f2850063542145c6f6a7
Piksi Megatama <alfrandhy@gmail.com>
2020-03-20 11:13:59 +0700

    first

---
+++ app/Views/home/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CyberCake App</title>
+
+    <link rel="stylesheet" href="<?php echo base_url();?>/assets/user/css/cake.css">
+</head>
+<body>
+    <nav class="clearfix">
+        <ul class="topnav">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+            <li class="icon">
+                <a href="javascript:void(0);" onclick="myFunction()">☰</a>
+            </li>
+        </ul>
+    </nav>
+
+    <script src="<?php echo base_url();?>/assets/user/js/cake.js"></script>
+</body>
+</html>
